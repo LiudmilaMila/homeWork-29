e37808fcc04f6e303a0df7642d19e33557c652d7
Mila <lydmiladerevjanko@gmail.com>
2022-01-08 20:11:48 +0200

    Реализованы элементы меню по созданию и удалению таблицы со студентами. Студент имеет минимум 4 поля.

--- src/main/java/com/speranskaya/Main.java
+++ src/main/java/com/speranskaya/Main.java
@@ -1,5 +1,9 @@
 package com.speranskaya;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Scanner;
 
 public class Main {
@@ -18,13 +22,22 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
+            System.out.println("Выберите из следующих действий : ");
+            System.out.println("\t1 - создать таблицу student.");
+            System.out.println("\t2 - удалить таблицу student.");
+            System.out.println("\t9 - выход.");
+
+            System.out.println("Сделайте свой выбор.");
+
             int userInput = Integer.parseInt(scanner.next());
             switch (userInput) {
                 case 1:
-                    System.out.println("Privet, horoshego dnya.");
+                    createStudentsTable();
+                    System.out.println("Таблица создана.");
                     break;
                 case 2:
-                    System.out.println("Precrasnaya pogoda,pravda?");
+                    deleteStudentsTable();
+                    System.out.println("Таблица удалена.");
                     break;
                 case 3:
                     System.out.println("Mmmm otlichnye duhi.");
@@ -45,7 +58,7 @@ public class Main {
                     System.out.println("Poehali na more)");
                     break;
                 case 9:
-                    System.out.println("Mozhno vzdremnut'");
+                    System.out.println("Пока");
                     return;
                 default:
                     System.out.println("Neraspoznannaya comanda");
@@ -55,4 +68,36 @@ public class Main {
         }
 
     }
+
+    private void createStudentsTable() {
+        try (Connection connection = DriverManager.getConnection("jdbc:sqlite:sample.db")) {
+            Statement statement = connection.createStatement();
+            statement.executeUpdate("CREATE TABLE student(id INTEGER, name VARCHAR(100)," +
+                    "age INTEGER, gender BOOL)");
+
+        } catch (SQLException e) {
+            e.printStackTrace(System.out);
+        }
+    }
+
+    private void deleteStudentsTable() {
+        try (Connection connection = DriverManager.getConnection("jdbc:sqlite:sample.db")) {
+            Statement statement = connection.createStatement();
+            statement.executeUpdate("DROP TABLE student");
+
+        } catch (SQLException e) {
+            e.printStackTrace(System.out);
+        }
+    }
+
+//    private void insertStudents(){
+//        try (Connection connection = DriverManager.getConnection("jdbc:sqlite:sample.db")) {
+//            Statement statement = connection.createStatement();
+//            statement.executeUpdate("INSERT TABLE student VALUES");
+//
+//        } catch (SQLException e) {
+//            e.printStackTrace(System.out);
+//        }
+//
+//    }
 }
